2859b63f9ee6f7eba928920840ba7bce93eabfb1
leoouyang08-code <leoouyang08@gmail.com>
2026-01-27 16:45:40 -0500

    e

---
+++ goober.py
@@ -0,0 +1,22 @@
+""" print('a')
+
+x = 5
+y = 7
+print(x + y)
+
+A = 12
+B = 7
+C = 20
+
+def isEven(num):
+    num = int(num)
+    if num % 2 == 0:
+        print("Your number is even!")
+    else:
+        print("Your number is odd!")
+
+num = input()        
+isEven(num) """
+
+name = input("Enter your name: ")
+print("Hello, " + name + "!")

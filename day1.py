86735c70e3ce71278f2f3b43f113e7bcc5d87184
NotLeo27 <leoouyang08@gmail.com>
2026-02-03 15:38:40 -0500

    yurrr

--- day1.py
+++ day1.py
@@ -45,6 +45,6 @@ name = input("Enter your name: ")
 print("Hello, " + name + "!")
 """
 
-name = input("Enter your name:")
-age = input("Enter your age:")
-print(name + "is " + age + " years old.")
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print(name + " is " + age + " years old.")

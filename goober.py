0c53d79941d0c6d1f63df17cb9e7a0ce61b1b4a9
NotLeo27 <leoouyang08@gmail.com>
2026-02-02 22:35:42 -0500

    ez work

--- goober.py
+++ goober.py
@@ -8,6 +8,24 @@ A = 12
 B = 7
 C = 20
 
+x = 10
+y = x + 20
+x = 30
+print(y)
+
+a = 12
+b = 7
+c = 20 
+
+if a>b:
+    m=a
+    if m>c:
+        m = m
+    else:
+        m = c
+else: 
+    m=b
+
 def isEven(num):
     num = int(num)
     if num % 2 == 0:
@@ -16,7 +34,12 @@ def isEven(num):
         print("Your number is odd!")
 
 num = input()        
-isEven(num) """
+isEven(num) 
+
+print("hello")
+non_number = 10/0
+print("world")
+"""
 
 name = input("Enter your name: ")
 print("Hello, " + name + "!")

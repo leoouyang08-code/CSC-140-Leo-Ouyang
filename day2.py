6b94c1451c4a3bca6a1e2303d2a16ea6611b936d
NotLeo27 <leoouyang08@gmail.com>
2026-02-03 15:36:38 -0500

    bruh

---
+++ day2.py
@@ -0,0 +1,9 @@
+M = True
+print(M, type(M))
+y = "hello"
+print(len(y))
+
+x = input("yo pass me an int between 17 and 25 exclusive: ")
+y = input("Please give me your age: ")
+age = int(x) + int(y)
+print("You will be a billionare at age " + age)

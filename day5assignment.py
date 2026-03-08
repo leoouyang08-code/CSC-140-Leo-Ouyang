e13fce13fa597adf01b71f38175facf760dc4720
leoouyang08-code <leoouyang08@gmail.com>
2026-03-08 00:10:45 -0500

    yoo

--- day5assignment.py
+++ day5assignment.py
@@ -1,4 +1,17 @@
 numlist = []
 
-while x != 0:
-    x = input("Hello ")
+integer_input = input("Input an integer; cannot be zero. ")
+
+while integer_input != 0:
+    try:
+        integer_input = input("Input an integer (0 to stop) ")
+        integer = int(integer_input) 
+        if integer_input != 0:
+            numlist.append(integer)
+        else:
+            break
+    
+    except ValueError:
+        print("Please enter a valid whole number.")
+
+print(numlist)

c907b5b457d1df319ff2101b26988eb97d7834ca
leoouyang08-code <leoouyang08@gmail.com>
2026-05-03 22:24:48 -0400

    bush

--- day11/day11assignment.py
+++ day11/day11assignment.py
@@ -1,11 +1,9 @@
-from sympy import (
-    symbols, expand, factor, simplify, diff, sin, cos, tan, ln, init_printing, expand_trig
-)
+from sympy import *
 init_printing()
 
-x = symbols('x')
+x, a, b = symbols('x a b')
 
-""" # 2. Differentiation Examples
+# No. 2:
 
 # Example A
 f = sin(x) * x**2
@@ -17,34 +15,45 @@ g = ln(x) / x
 print("\ng =", g)
 print("g' =", diff(g, x))
 
-#higher powers
-print("\nd/dx [x**10 · sin(x)] =", diff(x**10 * sin(x), x))
+# No. 3:
 
-# different functions
-print("\nd/dx [tan(x) · ln(x)]  =", diff(tan(x) * ln(x),  x))
-
-# combined terms
-print("\nd/dx [sin(x) + x**3 · cos(x)] =",
-      diff(sin(x) + x**3 * cos(x), x))
- """
-# 3. Algebraic Manipulation
-
-# Expand a binomial
+# expand
 expr1 = (x+2)**4
 print("\n(x + 2)**4 expanded =", expand(expr1))
 
-# Factor a polynomial
+# factor
 expr2 = x**3 - 4*x**2 + 4*x
 print("x³ - 4x² + 4x factored =", factor(expr2))
 
-# Simplify a rational expression
+# simplify
 expr3 = (x**2 - 1) / (x - 1)
 print("(x² - 1)/(x - 1) simplified =", simplify(expr3))
 
-# Other modifications
+# other changes
 print("\n(x + 3)**6 expanded =",  expand((x + 3)**6))
 print("x⁴ - 16 factored =",      factor(x**4 - 16))
 print("(x³ - 8)/(x - 2) simplified =", simplify((x**3 - 8)/(x - 2)))
 
-# More exploration
+# No. 4:
 
+# 20 mnis
+h = (sin(x)**3 * ln(x)**2) / (cos(x) * exp(x)) #
+print("\n--- Complex Derivative ---")
+print("h        =", h)
+print("h'       =", diff(h, x))
+print("\nh' (simplified) =", simplify(diff(h, x)))
+
+# first two are trig additional/subtratcion identities, and the others are sin triple identities
+print("sin(a + b) expanded =", expand_trig(sin(a + b)))
+
+print("cos(a - b) expanded =", expand_trig(cos(a - b)))
+
+print("\nsin(3a) expanded =", expand_trig(sin(3*a)))
+
+print("cos(3a) expanded =", expand_trig(cos(3*a)))
+
+"""
+Discovery: The most surprising result was that SymPy could instantly expand triple angle identities like sin(3a) = 3sin(a) - 4sin**3(a) and cos(3a) = 4cos**3(a) - 3cos(a) using the expand_trig command.
+These identities would usually take a lot of time and effort to derive normally, but it seems the library had it bulit in, which is useful. 
+I also found it impressive that SymPy handled the complex derivative of (sin³(x)*ln²(x)) / (cos(x)*eˣ) in seconds, a problem that would take ~20 minutes using quotient & product rules manually.
+"""
